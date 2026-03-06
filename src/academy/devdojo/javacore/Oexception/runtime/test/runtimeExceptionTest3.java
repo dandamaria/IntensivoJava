b9df4823550834e45d264c0bd270d1ee5ee7c5f3
Dandara <dandafarss@gmail.com>
2026-03-06 13:38:02 -0300

    feat: continuação dos estudos de exceções até a aula 100

---
+++ src/academy/devdojo/javacore/Oexception/runtime/test/runtimeExceptionTest3.java
@@ -0,0 +1,22 @@
+package academy.devdojo.javacore.Oexception.runtime.test;
+
+public class runtimeExceptionTest3 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("abrindo arquivo");
+            throw new RuntimeException();
+//            System.out.println("fechando arquivo liberado pelo SO");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("fechando arquivo liberado pelo SO");
+
+        }
+        return null;
+
+    }
+}
